8f02814878f63abbd88c899ff7680bdbe1040c79
Thibault Vlacich <thibault.vlacich@gmail.com>
2017-02-28 14:18:13 +0100

    Sessions : add extra

--- src/ShowSession.java
+++ src/ShowSession.java
@@ -2,6 +2,9 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -33,17 +36,76 @@ public class ShowSession extends HttpServlet {
 		PrintWriter w = response.getWriter();
 		
 		if (s.isNew()) {
-		  w.println("Welcome on my site");
+		  w.println("Welcome on my site. Please tell us a bit more about you :");
+		  
+		  w.println("<form method=\"POST\" action=\"\">");
+		  w.println("<label for=\"name\">Full name:</label>");
+		  w.println("<input type=\"text\" id=\"name\" name=\"name\">");
+	    
+		  w.println("<br>");
+	    
+		  w.println("<label for=\"birthdate\">Birth date (format dd/MM/yyyy):</label>");
+		  w.println("<input type=\"date\" id=\"birthdate\" name=\"birthdate\">");
+	    
+		  w.println("<br>");
+
+		  w.println("<input type=\"submit\" value=\"Submit\">");
+		  w.println("</form>");
+		  
 		  s.setAttribute("numberOfVisits", 1);
 		} else {
 	    int numberOfVisits = (int) s.getAttribute("numberOfVisits");
 	    numberOfVisits++;
 	    
-	    s.setAttribute("numberOfVisits", numberOfVisits);
+	    String name = (String) s.getAttribute("name");
+	    String birthdate = (String) s.getAttribute("birthdate");
 	    
-		  w.println("Welcome back!");
+	    s.setAttribute("numberOfVisits", numberOfVisits);
+		  
+		  if (name != null) {
+		    w.println("Hi " + name + "!");
+		  } else {  
+	      w.println("Welcome back!");
+		  }
+		  
+		  if (birthdate != null) {
+		    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		    
+		    try {
+		      Date today = new Date();
+          Date date = formatter.parse(birthdate);
+          
+          int currentDays = absoluteDay(today.getMonth(), today.getDay());
+          int birthDayDays1 = absoluteDay(date.getMonth(), date.getDay());
+          
+          int numberOfDays = (birthDayDays1 - currentDays >= 0) ? (birthDayDays1 - currentDays) : (365 - (currentDays - birthDayDays1));
+          
+          w.println("There are " + numberOfDays + " days to your birthday.");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		  }
+		  
 		  w.println("Its the " + numberOfVisits + " times you've come on my site!");
 		}
 	}
 
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    HttpSession s = request.getSession(true);
+	    PrintWriter w = response.getWriter();
+	 
+	    String name = request.getParameter("name");
+	    String birthDate = request.getParameter("birthdate");
+
+	    s.setAttribute("name", name);
+	    s.setAttribute("birthdate", birthDate);
+	    
+	    w.println("<a href=\"\">< Retour</a>");
+	 }
+	 
+   private static int absoluteDay(int month, int day){
+     int[] days = {0, 0, 31, 60, 91, 121, 91, 121, 152, 182, 213, 244, 274, 305, 335};
+     
+     return days[month] + day;
+ }
 }
